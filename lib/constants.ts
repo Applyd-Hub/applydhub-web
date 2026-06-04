ec0684ff0a7b7e8e5d751d6bc1998cc7937c1b60
Afoojnr <afolabiabdulhamid4@gmail.com>
2026-06-04 12:25:00 +0200

    refactor: update google play link

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@ export const APP_CONSTANTS = {
   email: "hello@applydhub.com",
   baseUrl: "https://www.applydhub.com",
   appStore: "https://apps.apple.com/app/id6765480348",
-  googlePlay: "https://play.google.com/apps/testing/com.afooinc.applydhub",
+  googlePlay: "https://play.google.com/store/apps/details?id=com.afooinc.applydhub",
 } as const;
